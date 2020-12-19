a2f0111008e36481dc95b80b2e191362592ae6a6
MoSaif00 <moh14ab@gmail.com>
2020-12-19 23:51:45 +0100

    Updates

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const formElement = document.getElementById('form');
-formElement.addEventListener('submit', memorizeMarker);
+formElement.addEventListener('submit', memorizeMarkers);
 
-function memorizeMarker() {
-  const websiteName = document.getElementById('website-name').value;
-  const websiteURL = document.getElementById('website-url').value;
-  const errorMessage = document.getElementById('error-message');
+function memorizeMarkers(x) {
+  let websiteName = document.getElementById('website-name').value;
+  let websiteURL = document.getElementById('website-url').value;
+  let errorMessage = document.getElementById('error-message');
+  x.preventDefault();
   if (!websiteURL && !websiteName) {
     errorMessage.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-    <strong>OH SORRY</strong> You have not entered the website name and URL. 
+    <strong>OH SORRY</strong> You have not entered the website name and URL.
   </div>`;
     return false;
   } else if (!websiteName) {
@@ -30,38 +31,50 @@ function memorizeMarker() {
     SiteName: websiteName,
     SiteURL: websiteURL,
   };
+
   if (localStorage.getItem('storage') === null) {
-    let arr = [];
-    arr.push(markers);
-    const myJson = JSON.stringify(arr);
-    localStorage.setItem('storage', myJson);
+    let sitemarker = [];
+    sitemarker.push(markers);
+    const myJSON = JSON.stringify(sitemarker);
+    localStorage.setItem('storage', myJSON);
   } else {
-    const get = JSON.parse(localStorage.getItem('storage'));
+    let get = JSON.parse(localStorage.getItem('storage'));
     get.push(markers);
-    myJson = JSON.stringify(get);
-    localStorage.setItem('storage', myJson);
+    let myJSON = JSON.stringify(get);
+    localStorage.setItem('storage', myJSON);
   }
+  displayMarkerList();
+  document.getElementById('form').reset();
+  // document.getElementById('error-message').reset();
 }
 
 function displayMarkerList() {
-  const get = JSON.parse(localStorage.getItem('storage'));
-  const resultOfTheList = document.getElementById('websites-list');
+  let get = JSON.parse(localStorage.getItem('storage'));
+  let resultOfTheList = document.getElementById('websites-list');
   resultOfTheList.innerHTML = '';
   for (let i = 0; i < get.length; i++) {
-    resultOfTheList.innerHTML += `
-        <li class="d-block mb-2">${get[i].SiteName}<a class="btn btn-primary ml-4 p-1" href="${get[i].SiteURL}" target="_blank">Open </a> <button class="btn btn-danger ml-2 p-1">Delete</button></li>
-    `;
+    resultOfTheList.innerHTML +=
+      '<div class="d-block mb-2">' +
+      get[i].SiteName +
+      '<a class="btn btn-primary ml-4 p-1" href="' +
+      get[i].SiteURL +
+      '" target="_blank">Go</a>' +
+      '<button onclick="deleteTheMarker(\'' +
+      get[i].SiteName +
+      '\')" target="_blank" class="btn btn-danger ml-2 p-1">Delete</button>' +
+      '</div>';
   }
 }
-displayMarkerList();
+// displayMarkerList();
 
 function deleteTheMarker(element) {
-  const get = JSON.parse(localStorage.getItem('storage'));
+  let get = JSON.parse(localStorage.getItem('storage'));
   for (let i = 0; i < get.length; i++) {
     if (get[i].SiteName === element) {
       get.splice(i, 1);
     }
   }
-
-  myJson = JSON.stringify(get);
+  let myJSON = JSON.stringify(get);
+  localStorage.setItem('storage', myJSON);
+  displayMarkerList();
 }
